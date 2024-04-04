07d6d4220eb52418c4c22b9c001a1afefaacc047
Francis Tate Chekure <ftchekure@gmail.com>
2024-04-04 15:45:07 +0200

    first commit

---
+++ src/main/java/com/designtartans/pigfarmingserver/PigFarmingServerApplication.java
@@ -0,0 +1,13 @@
+package com.designtartans.pigfarmingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigFarmingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigFarmingServerApplication.class, args);
+    }
+
+}

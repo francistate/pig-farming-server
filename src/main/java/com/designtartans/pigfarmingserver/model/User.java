b8bf23527145347c6deb2cb441374175b3270bb8
Regis-Ntwari <rex90danny@gmail.com>
2024-04-08 10:42:04 +0200

    change table name of users

--- src/main/java/com/designtartans/pigfarmingserver/model/User.java
+++ src/main/java/com/designtartans/pigfarmingserver/model/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

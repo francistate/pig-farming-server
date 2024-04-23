ae2f88fa3824d8141dd7beb8874df0768ec5e880
Regis-Ntwari <rex90danny@gmail.com>
2024-04-23 16:10:48 +0200

    fix imports

--- src/main/java/com/designtartans/pigfarmingserver/repository/PigWeightRecordRepository.java
+++ src/main/java/com/designtartans/pigfarmingserver/repository/PigWeightRecordRepository.java
@@ -6,10 +6,7 @@ import com.designtartans.pigfarmingserver.model.PigWeightRecord;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PigWeightRecordRepository extends JpaRepository<PigWeightRecord, Long> {
